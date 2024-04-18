44408ea8631c6baf752ef26baada8e382f1f53b1
Khaleel <khaleelanjum0@gmail.com>
2024-04-18 22:04:55 +1000

    init

---
+++ updateScript.lua
@@ -0,0 +1,32 @@
+local scriptToUpdate = ""
+
+local function getScriptName(name)
+	local urlArray = {}
+	for word in string.gmatch(name, "([^*/]+)") do
+		table.insert(urlArray, word)
+	end
+	return urlArray[#urlArray]
+end
+
+local function updateFile(filepath, content)
+	local file = fs.open(filepath, "w")
+	file.write(content)
+	file.close()
+end
+
+local function updateScript()
+	local request = http.get(scriptToUpdate);
+	local content = request.readAll();
+	local scriptName = getScriptName(scriptToUpdate);
+	local filepath = fs.combine("/startup",scriptName)
+	request.close()
+	updateFile(filepath, content)
+	print("File saved to " .. filepath)
+	shell.execute(filepath)
+end
+
+local function start()
+	updateScript()
+end
+
+start()
